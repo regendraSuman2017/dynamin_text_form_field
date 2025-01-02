519cc3ba469eb6387ec7990fadc7c05e4f1af895
Regendra Suman <regendrasuman2017@gmail.com>
2025-01-02 10:48:05 +0530

    create button

--- lib/features/registraction_form/view/registration_view_page.dart
+++ lib/features/registraction_form/view/registration_view_page.dart
@@ -38,7 +38,7 @@ class _DynamicRegistrationWidgetState extends State<DynamicRegistrationWidget> {
   List<GlobalKey<FormFieldState>> _fieldKeys = [];
 
   // List of FocusNodes to manage focus on each field
-  List<FocusNode> _focusNodes = [];
+
 
   @override
   void initState() {
@@ -50,14 +50,13 @@ class _DynamicRegistrationWidgetState extends State<DynamicRegistrationWidget> {
   @override
   Widget build(BuildContext context) {
     // Create a FocusNode for each field
-    FocusNode focusNode = FocusNode();
-    _focusNodes.add(focusNode);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dynamic Registration Fields with BLoC'),
+        title: Text('Dynamic Registration Fields with BLoC',style: TextStyle(fontSize: 14),),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(8.0),
         child: BlocBuilder<RegistrationBloc, RegistrationState>(
           builder: (context, state) {
             if (state is RegistrationInitialState) {
@@ -67,85 +66,131 @@ class _DynamicRegistrationWidgetState extends State<DynamicRegistrationWidget> {
             }
             if (state is RegistrationFieldsState) {
               // Update the list of field keys and focus nodes based on the number of controllers
-              _fieldKeys = List.generate(
-                  state.controllers.length, (index) => GlobalKey<FormFieldState>());
-              _focusNodes = List.generate(
-                  state.controllers.length, (index) => FocusNode());
+              _fieldKeys = List.generate(state.controllers.length, (index) => GlobalKey<FormFieldState>());
 
-              return Form(
-                key: _formKey, // Assign the global key here
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.7,
-                  margin: EdgeInsets.all(5),
-                  child: ListView.builder(
-                    controller: _scrollController, // Controller for scrolling
-                    itemCount: state.controllers.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TextFormField(
-                                controller: state.controllers[index],
-                                key: _fieldKeys[index], // Assign GlobalKey here
-                                focusNode: _focusNodes[index], // Assign FocusNode here
-                                decoration: InputDecoration(
-                                  labelText: 'Field ${index + 1}',
-                                  border: OutlineInputBorder(),
+              return Column(
+                children: [
+                  Form(
+                    key: _formKey, // Assign the global key here
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.7,
+                      margin: EdgeInsets.all(5),
+                      child: ListView.builder(
+                        controller: _scrollController, // Controller for scrolling
+                        itemCount: state.controllers.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: TextFormField(
+                                    controller: state.controllers[index],
+                                    key: _fieldKeys[index], // Assign GlobalKey here
+                                    decoration: InputDecoration(
+                                      labelText: 'Field ${index + 1}',
+                                      border: OutlineInputBorder(),
+                                    ),
+                                    // Validator to check if the field is empty
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
                                 ),
-                                // Validator to check if the field is empty
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter some text';
-                                  }
-                                  return null;
-                                },
-                              ),
+                                IconButton(
+                                  icon: Icon(Icons.delete_outline_sharp, color: Colors.red),
+                                  onPressed: () {
+                                    BlocProvider.of<RegistrationBloc>(context).add(RemoveFieldEvent(index));
+                                    // Remove the corresponding GlobalKey and FocusNode when a field is removed
+                                    _fieldKeys.removeAt(index);
+                                  },
+                                ),
+                              ],
                             ),
-                            IconButton(
-                              icon: Icon(Icons.remove_circle, color: Colors.red),
-                              onPressed: () {
-                                BlocProvider.of<RegistrationBloc>(context)
-                                    .add(RemoveFieldEvent(index));
-                                // Remove the corresponding GlobalKey and FocusNode when a field is removed
-                                _fieldKeys.removeAt(index);
-                                _focusNodes.removeAt(index);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    },
+                          );
+                        },
+                      ),
+                    ),
                   ),
-                ),
+
+                ],
               );
             }
             return Container();
           },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add a new field when button is clicked
-          BlocProvider.of<RegistrationBloc>(context).add(AddFieldEvent());
-        },
-        backgroundColor: Colors.blue,
-        child: Icon(Icons.add, size: 30),
-      ),
-      bottomNavigationBar: ElevatedButton(
-        onPressed: () {
-          // Validate the form
-          if (_formKey.currentState?.validate() ?? false) {
-            // If form is valid, submit it
-            BlocProvider.of<RegistrationBloc>(context)
-                .add(SubmitRegistrationEvent());
-          } else {
-            // If form is invalid, scroll to the first invalid field
-            _scrollToFirstInvalidField();
-          }
-        },
-        child: Text('Submit'),
+
+      bottomNavigationBar: Row(
+        children: [
+          Expanded(
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.blueGrey), // Fixed the backgroundColor declaration
+                padding: WidgetStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0)), // Add padding for better visual spacing
+                elevation: WidgetStateProperty.all<double>(5), // Add elevation for a shadow effect
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6.0), // Rounded corners
+                  ),
+                ),
+                side: WidgetStateProperty.all<BorderSide>(BorderSide(color: Colors.blueGrey, width: 1)), // Optional: Border with color
+              ),
+              onPressed: () {
+                // Validate the form
+                if (_formKey.currentState?.validate() ?? false) {
+                  // If form is valid, submit it
+                  context.read<RegistrationBloc>().add(SubmitRegistrationEvent());
+                } else {
+                  _scrollToFirstInvalidField();
+                }
+              },
+              child: Text(
+                'Submit Form',
+                style: TextStyle(
+                  color: Colors.white, // Text color for better contrast
+                  fontWeight: FontWeight.bold, // Bold text for emphasis
+                  fontSize: 16.0, // Optional: Adjust font size
+                ),
+              ),
+            )
+
+          ),
+SizedBox(width: 10,),
+          Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all<Color>(Colors.green), // Fixed the backgroundColor declaration
+                  padding: WidgetStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0)), // Add padding for better visual spacing
+                  elevation: WidgetStateProperty.all<double>(5), // Add elevation for a shadow effect
+                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6.0), // Rounded corners
+                    ),
+                  ),
+                  side: WidgetStateProperty.all<BorderSide>(BorderSide(color: Colors.blueGrey, width: 1)), // Optional: Border with color
+                ),
+                onPressed: () {
+
+                    // If form is valid, submit it
+                    context.read<RegistrationBloc>().add(AddFieldEvent());
+                },
+                child: Text(
+                  'Add More Field',
+                  style: TextStyle(
+                    color: Colors.white, // Text color for better contrast
+                    fontWeight: FontWeight.bold, // Bold text for emphasis
+                    fontSize: 16.0, // Optional: Adjust font size
+                  ),
+                ),
+              )
+
+          ),
+
+        ],
       ),
     );
   }
@@ -155,7 +200,7 @@ class _DynamicRegistrationWidgetState extends State<DynamicRegistrationWidget> {
     final formState = _formKey.currentState;
     if (formState != null) {
       for (int index = 0; index < _fieldKeys.length; index++) {
-        final focusNode = _focusNodes[index];
+        // final focusNode = _focusNodes[index];
         // Check if the field at the current index is invalid
         final fieldState =  _fieldKeys[index].currentState!.validate();
         print("sdjskdh ${fieldState}");
